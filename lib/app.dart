45858b169ad12f7eaa446a86d505b8b5f9eed932
Davoleo <davoleo2001@gmail.com>
2020-08-12 12:12:30 +0200

    Initial Commit
    + theme colors

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:daily_quest/ui/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFF6d4c41),
+        primaryColorDark: Color(0xFF40241a),
+        primaryColorLight: Color(0xFF9c786c),
+        accentColor: Color(0xFFce7e00)
+      ),
+      home: HomePage(),
+    );
+  }
+}
