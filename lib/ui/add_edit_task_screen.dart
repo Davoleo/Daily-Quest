8b0ecf2f8990d1fb023cadb5513999a94dc6bbf7
Davoleo <davoleo2001@gmail.com>
2021-02-11 18:56:18 +0100

    Work on New/Edit Task UI

---
+++ lib/ui/add_edit_task_screen.dart
@@ -0,0 +1,96 @@
+import 'package:daily_quest/model/Task.dart';
+import 'package:daily_quest/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class AddEditTaskScreen extends StatefulWidget {
+
+  final bool edit;
+  final Task previousTask;
+
+  final List<DropdownMenuItem<TaskFrequency>> _categories = [];
+
+  AddEditTaskScreen(this.edit, [this.previousTask]) {
+    for (TaskFrequency freq in TaskFrequency.values) {
+      String catName = freq.toString().split('.').last;
+      _categories.add(new DropdownMenuItem(child: Text(catName)));
+    }
+  }
+
+  @override
+  _AddEditTaskScreenState createState() => _AddEditTaskScreenState();
+}
+
+class _AddEditTaskScreenState extends State<AddEditTaskScreen> {
+
+  TaskFrequency category;
+
+  @override
+  Widget build(BuildContext context) {
+
+    Container content = Container(
+      color: Constants.primaryLight30,
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Wrap(
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          runSpacing: 20,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: Constants.fieldOutline,
+                labelText: "Title",
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              minLines: 3,
+              decoration: InputDecoration(
+                border: Constants.fieldOutline,
+                labelText: "Notes",
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Icon: ")
+                //TODO Icon Selector Component
+              ],
+            ),
+            DropdownButton<TaskFrequency>(
+              underline: Divider(thickness: 2, color: Theme.of(context).primaryColor,),
+              value: category,
+              items: widget._categories,
+              //Old Alternative
+              // TaskFrequency.values
+              //     .map<DropdownMenuItem<TaskFrequency>>(
+              //         (TaskFrequency freq) => DropdownMenuItem(child: Text(freq.toString().split('.').last))
+              onChanged: (value) {
+                setState(() {
+                  category = value;
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+
+    if (!widget.edit) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("New Task"),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.done),
+                onPressed: () => {}
+            ),
+          ],
+        ),
+        body: content,
+      );
+    }
+
+    return Container();
+  }
+}
